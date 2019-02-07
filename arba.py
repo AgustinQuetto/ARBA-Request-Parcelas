3f9f812d7702410c115919d3bc800deaa52e3170
Agustín Quetto <agustinquetto@gmail.com>
2019-02-07 15:32:54 -0300

    .

--- arba.py
+++ arba.py
@@ -15,30 +15,32 @@ ssn.cookies.update({'JSESSIONID': secondCookie})
 print(ssn.cookies)
 """
 
-file = open('parcelas.txt','a')
+_file = open('parcelas.txt','a')
 log = open('log.txt', 'a')
-startCookie = input('Ingrese el cookie:')
+startCookie = ""
 
-for i in range(58413,150000):
+for i in range(0,200000):
   try:
-    reqTwo = ssn.get('http://carto.arba.gov.ar/cartoArba2/client/getPdoPda?pdo=118&pda='+str(i)+'&epsg=4326', cookies={'JSESSIONID': startCookie})
+    reqTwo = ssn.get('http://www.carto.arba.gov.ar/cartoArba/client/getPdoPda?pdo=63&pda='+str(i)+'&epsg=4326', cookies={'JSESSIONID': startCookie}, verify=False)
     isFound = json.loads(reqTwo.text.encode('utf-8'))
+    print(reqTwo.text.encode('utf-8'))
     if isFound['found'] == True:
-      log.write('ok: %d' % i)
-      file.write(reqTwo.text + '\n')
+      log.write('\nok %d' % i)
+      print(reqTwo.text + '\n')
+      _file.write(reqTwo.text + '\n')
     else:
-      log.write('no: %d' % i)
+      log.write('\nno %d' % i)
   except:
-    startCookie = input('Ingrese el cookie:')
-    reqTwo = ssn.get('http://carto.arba.gov.ar/cartoArba2/client/getPdoPda?pdo=118&pda='+str(i)+'&epsg=4326', cookies={'JSESSIONID': startCookie})
+    startCookie = ""
+    reqTwo = ssn.get('http://www.carto.arba.gov.ar/cartoArba/client/getPdoPda?pdo=63&pda='+str(i)+'&epsg=4326', cookies={'JSESSIONID': startCookie}, verify=False)
     isFound = json.loads(reqTwo.text.encode('utf-8'))
     if isFound['found'] == True:
       log.write('ok: %d' % i)
-      file.write(reqTwo.text + '\n')
+      _file.write(reqTwo.text + '\n')
     else:
       log.write('no: %d' % i)
 
-file.close()
+_file.close()
 log.close()
 
 print('-----FINISH-----')
